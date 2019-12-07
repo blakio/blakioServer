18e3c73ce5ba9bffd110294f08b99c4c2fb86869
Isaiah Harrison <ijh5005@outlook.com>
2019-12-07 13:33:31 -0500

    code clean up

--- app.js
+++ app.js
@@ -16,33 +16,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(config.PORT, console.log(`app running on port ${config.PORT}`));
-
-
-// const mongoose = require("mongoose");
-// const connectionString = `mongodb+srv://blakio:blakio@blakioepscluster-9dqqq.mongodb.net/test?retryWrites=true&w=majority`;
-// mongoose.connect(connectionString, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// mongoose.connection.on("connected", () => console.log("connected to mongodb"));
-//
-// const Schema = mongoose.Schema;
-// const EmployeeSchema = new Schema({
-//   name: String,
-//   jobTitle: String
-// })
-// const Employee = mongoose.model("employee", EmployeeSchema);
-//
-// const data = {
-//   name: "Isaiah",
-//   jobTitle: "Full Stack"
-// }
-//
-// const employee = new Employee(data);
-// employee.save(err => {
-//   if(err){
-//     console.log("error")
-//   } else {
-//     console.log("success")
-//   }
-// })
