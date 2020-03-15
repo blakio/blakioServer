425d5742c2c971b5d2e640feb2db3930bf0f7212
Isaiah Harrison <ijh5005@outlook.com>
2020-03-15 17:36:57 -0400

    mongo db config to env

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 5000,
-  mongodb: {
-    user: "blakio",
-    mongodbpw: "blakio"
-  },
   connectionRoute: "./eps/index.js"
 }
--- servers/eps/models/index.js
+++ servers/eps/models/index.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const connectionString = `mongodb+srv://blakio:blakio@blakioepscluster-9dqqq.mongodb.net/test?retryWrites=true&w=majority`;
+const user = process.env.MONGODB_USER
+const password = process.env.MONGODB_PASSWORD
+const connectionString = `mongodb+srv://${user}:${password}@blakioepscluster-9dqqq.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(connectionString, () => console.log("connected successfully to mongodb"));
 mongoose.connection.on("connected", () => console.log("connected to mongodb"));
